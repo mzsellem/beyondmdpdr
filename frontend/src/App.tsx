6e637234f79f5ae19d5498a1c44bb4b1f8a395ff
mzsellem <marleysellem@gmail.com>
2024-03-01 14:48:19 -0500

    test that front and back ends are connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/react.svg';
+import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/test/')
+      .then(res => res.json())
+      .then(data => setData(data.data));
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>An Awesome Blog </h1>
+        <h3>On Django, React, Postgres, and Docker </h3>
+
+        <p>{data}</p>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;

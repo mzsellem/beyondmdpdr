85f7f1cab28397ea9ac8bdfb11de73bf095321a2
mzsellem <marleysellem@gmail.com>
2024-03-05 16:04:28 -0500

    change everything to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'beyondmdpatientdb',
-        'USER': 'marleysellem',
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432',
